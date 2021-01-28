325e81c0276ec456de880848ec61e9989ceb7e12
luhs <luhs@cadg.cn>
2021-01-28 16:30:03 +0800

    ''

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV
-module.exports = {
-  lintOnSave: false,
-  configureWebpack: config=>{
-    config = require(`./webpackConfig/${env}/vue.config.js`)
-    return  config
-  },
-  publicPath: './'
-}
