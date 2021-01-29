2b62bdb38710983951f3c6d5ac58a1c3eee636e8
luhs <luhs@cadg.cn>
2021-01-29 17:54:54 +0800

    简单框架

--- src/views/layer/layer.vue
+++ src/views/layer/layer.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <layer-header></layer-header>
-        <layer-content></layer-content>
-        <layer-footer></layer-footer>
+    <div class="ownerPages">
+        <layer-header class="header"></layer-header>
+        <layer-content class="content"></layer-content>
+        <layer-footer class="footer"></layer-footer>
     </div>
 </template>
 
@@ -16,6 +16,31 @@
       };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+ .ownerPages{
+     width:100%;
+     height: 100%;
+     background: transparent url('../../../public/bg-03.png') no-repeat fixed  ;
+     .header{
+        position: absolute;
+         background-color: gray;
+         left: 0;
+         top: 0;
+         height: 3em;
+         width: 100%;
+     }
+     .content{
+       width: 100%;
+         height: 100%;
+         background-color: orange;
+         padding: 3em 0 3em 0;
+     }
+     .footer{
+         position: absolute;
+         background-color: black;
+         bottom: 0;
+         height: 3em;
+         width: 100%;
+     }
+ }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
 const env = process.env.NODE_ENV
+const path = require('path')
 module.exports = {
   lintOnSave: false,
   configureWebpack: config=>{
     config = require(`./webpackConfig/${env}/vue.config.js`)
     return  config
   },
-  publicPath: './'
+  publicPath: './',
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, './src'))
+      .set('@home',path.resolve(__dirname))
+  }
 }
